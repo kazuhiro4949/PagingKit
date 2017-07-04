7adeeffbd287b355c7af945badfd7863be10bee1
kahayash <kahayash@MBP15UAQ-A022.local>
2017-07-04 21:45:50 +0900

    fixup

--- PagingKit/PagingMenuViewController.swift
+++ PagingKit/PagingMenuViewController.swift
@@ -144,7 +144,9 @@ public class PagingMenuViewController: UIViewController {
         
         if let index = index {
             collectionView.performBatchUpdates(nil) { [weak self] (_) in
+                guard let _self = self else { return }
                 self?.scroll(index: index, percent: 0, animated: false)
+                self?.delegate?.menuViewController(viewController: _self, didSelect: index, previousPage: 0)
             }
         }
     }
