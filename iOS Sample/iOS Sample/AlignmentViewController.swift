8e030fa5b48bac99630f569e90021e25b576a3c1
Kazuhiro Hayashi <kazuhiro4949@gmail.com>
2018-02-12 15:07:25 +0900

    add sample code

---
+++ iOS Sample/iOS Sample/AlignmentViewController.swift
@@ -0,0 +1,124 @@
+//
+//  DynamicSizeViewController.swift
+//  iOS Sample
+//
+//  Copyright (c) 2017 Kazuhiro Hayashi
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import UIKit
+import PagingKit
+
+class AlignmentViewController: UIViewController {
+    
+    var menuViewController: PagingMenuViewController?
+    var contentViewController: PagingContentViewController?
+    
+    static var sizingCell = TitleLabelMenuViewCell(frame: CGRect(x: 0, y: 0, width: 1, height: 1))
+    
+    
+    let dataSource: [(menu: String, content: UIViewController)] = ["Martinez", "Alfred", "Louis"].map {
+        let title = $0
+        let vc = UIStoryboard(name: "ContentTableViewController", bundle: nil).instantiateInitialViewController() as! ContentTableViewController
+        return (menu: title, content: vc)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuViewController?.register(type: TitleLabelMenuViewCell.self, forCellWithReuseIdentifier: "identifier")
+        menuViewController?.registerFocusView(view: UnderlineFocusView())
+        menuViewController?.reloadData()
+        contentViewController?.reloadData()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? PagingMenuViewController {
+            menuViewController = vc
+            menuViewController?.dataSource = self
+            menuViewController?.delegate = self
+        } else if let vc = segue.destination as? PagingContentViewController {
+            contentViewController = vc
+            contentViewController?.delegate = self
+            contentViewController?.dataSource = self
+        }
+    }
+    
+    @IBAction func updateSizeButtonDidTap(_ sender: UIBarButtonItem) {
+        let size = UIScreen.main.bounds.size
+        let width =  size.width * CGFloat(arc4random_uniform(100)) / 100
+        let height = size.height * CGFloat(arc4random_uniform(100)) / 100
+        
+        let updateSize = height <= 108 ? .zero : CGSize(width: width, height: height)
+        UIView.perform(.delete, on: [], options: UIViewAnimationOptions(rawValue: 0), animations: { [weak self] in
+            self?.view.frame.size = updateSize
+            self?.view.setNeedsLayout()
+            self?.view.layoutIfNeeded()
+            }, completion: { (finish) in })
+    }
+    
+}
+
+extension AlignmentViewController: PagingMenuViewControllerDataSource {
+    func menuViewController(viewController: PagingMenuViewController, cellForItemAt index: Int) -> PagingMenuViewCell {
+        let cell = viewController.dequeueReusableCell(withReuseIdentifier: "identifier", for: index)  as! TitleLabelMenuViewCell
+        cell.titleLabel.text = dataSource[index].menu
+        return cell
+    }
+    
+    func menuViewController(viewController: PagingMenuViewController, widthForItemAt index: Int) -> CGFloat {
+        DynamicSizeViewController.sizingCell.titleLabel.text = dataSource[index].menu
+        var referenceSize = UILayoutFittingCompressedSize
+        referenceSize.height = viewController.view.bounds.height
+        let size = DynamicSizeViewController.sizingCell.systemLayoutSizeFitting(referenceSize)
+        return size.width
+    }
+    
+    
+    func numberOfItemsForMenuViewController(viewController: PagingMenuViewController) -> Int {
+        return dataSource.count
+    }
+}
+
+extension AlignmentViewController: PagingContentViewControllerDataSource {
+    func numberOfItemsForContentViewController(viewController: PagingContentViewController) -> Int {
+        return dataSource.count
+    }
+    
+    func contentViewController(viewController: PagingContentViewController, viewControllerAt Index: Int) -> UIViewController {
+        return dataSource[Index].content
+    }
+}
+
+extension AlignmentViewController: PagingMenuViewControllerDelegate {
+    func menuViewController(viewController: PagingMenuViewController, didSelect page: Int, previousPage: Int) {
+        contentViewController?.scroll(to: page, animated: true)
+    }
+}
+
+extension AlignmentViewController: PagingContentViewControllerDelegate {
+    func contentViewController(viewController: PagingContentViewController, didManualScrollOn index: Int, percent: CGFloat) {
+        menuViewController?.scroll(index: index, percent: percent, animated: false)
+    }
+}
+
