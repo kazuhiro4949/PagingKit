05d9b516e3bad5d7321ab628c0544521b3ea96c0
Kazuhiro Hayashi <kazuhiro.hayashi.49@gmail.com>
2020-02-09 12:17:35 +0900

    add host application for unit test

---
+++ UnitTestHostApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestHostApplication
+//
+//  Created by kahayash on 2/9/2 R.
+//  Copyright © 2 Reiwa Kazuhiro Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
